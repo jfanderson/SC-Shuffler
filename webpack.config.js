da63b386ccf40a023c31e31c2751c72f47d4dc2a
John Anderson <jfanderson373@gmail.com>
2017-05-14 02:59:54 -0700

    Set up dev environment

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist/'
+  },
+  module: {
+    loaders: [
+      { test: /\.(js|jsx)$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ]
+  },
+  devtool: "cheap-eval-source-map"
+};
